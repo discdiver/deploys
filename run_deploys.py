ea3aef424dc67a9e782ca98a604b76d50cd4a629
Jeff Hale <jeffmshale@gmail.com>
2024-01-23 13:31:14 -0500

    change

--- run_deploys.py
+++ run_deploys.py
@@ -9,7 +9,7 @@ def fetch_temps(lat: float = 38.9, lon: float = -77.0):
         "https://api.open-meteo.com/v1/forecast/",
         params=dict(latitude=lat, longitude=lon, hourly="temperature_2m"),
     )
-    forecasted_temps = float(weather.json()["hourly"]["temperature_2m"][:12])
+    forecasted_temps = weather.json()["hourly"]["temperature_2m"][:12]
     print(f"Max expected value in the next 12 hours: {max(forecasted_temps)} degrees C")
     return
 
