5b4ea7f842933ac4e02054ff09f845ae2f032aa9
Jeff Hale <jeffmshale@gmail.com>
2024-01-23 14:27:14 -0500

    rename

--- flow-code/fetch_data.py
+++ flow-code/fetch_data.py
@@ -9,5 +9,5 @@ def fetch_temps(lat: float = 38.9, lon: float = -77.0):
         params=dict(latitude=lat, longitude=lon, hourly="temperature_2m"),
     )
     forecasted_temps = weather.json()["hourly"]["temperature_2m"][:12]
-    print(f"Expected temps for the next 12 hours: {"forecasted_temps"} degrees C")
+    print(f"Expected temps for the next 12 hours: {forecasted_temps} degrees C")
     return forecasted_temps
