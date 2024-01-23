47518567bbd405e7f50d9911d662f73df1c2d010
Jeff Hale <jeffmshale@gmail.com>
2024-01-23 15:35:06 -0500

    fix typo

--- flow-code/run_deployment.py
+++ flow-code/run_deployment.py
@@ -10,7 +10,7 @@ def fetch_temps(lat: float = 38.9, lon: float = -77.0):
         params=dict(latitude=lat, longitude=lon, hourly="temperature_2m"),
     )
     forecasted_temps = weather.json()["hourly"]["temperature_2m"][:12]
-    print(f"Expected temps for the next 12 hours (degrees C): {forecasted_temps}"
+    print(f"Expected temps for the next 12 hours (degrees C): {forecasted_temps}")
 
     run_deployment(
         name="train-model/train-model",
